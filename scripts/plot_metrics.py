fb9cfb7659cb8fc5e11c7a647d4ce544ffc02265
Dabrze <dariusz.brzezinski@cs.put.poznan.pl>
2020-12-02 14:01:17 +0100

    Real world datasets added

--- scripts/plot_metrics.py
+++ scripts/plot_metrics.py
@@ -41,12 +41,19 @@ lines_styles = cycle([
 
 def plot_metrics(input_dir, output_dir):
 	x_label = 'learning evaluation instances'
-	classifiers = {os.path.splitext(entry.name)[0]: pd.read_csv(entry.path, skiprows=range(1, 1 + args.s))
-				   for entry in os.scandir(input_dir)}
+	classifiers = {}
+
+	for entry in os.scandir(input_dir):
+		classifier_name = os.path.splitext(entry.name)[0]
+		classifier_evaluation_df = pd.read_csv(entry.path, skiprows=range(1, 1 + args.s), na_values=["?"])
+		classifier_evaluation_df = classifier_evaluation_df.dropna()
+		classifiers[classifier_name] = classifier_evaluation_df
+
 	for metric in args.metrics:
 		ax = plt.gca()
-		for point_name in [' ', 'start', 'pre-dirft', 'post-drift', 'end']:
-			ax.plot([], [], ' ', label=point_name)
+		if args.d:
+			for point_name in [' ', 'start', 'pre-dirft', 'post-drift', 'end']:
+				ax.plot([], [], ' ', label=point_name)
 		for name in classifiers.keys() if args.o is None else args.o:
 			data = classifiers[name]
 
@@ -58,13 +65,15 @@ def plot_metrics(input_dir, output_dir):
 				y_label = 'rolling_' + metric
 				data[y_label] = data[metric].rolling(window=args.a, min_periods=1).mean()
 			ax = data.plot(**next(lines_styles), x=x_label, y=y_label, label=name, ax=ax, figsize=(7, 5))
-			legend_data = [
-				data[y_label].iloc[0],
-				data.loc[data[x_label] == args.d[0], y_label].item(),
-				data.loc[data[x_label] == args.d[1], y_label].item(),
-				data[y_label].iloc[-1]]
-			for value in legend_data:
-				ax.plot([], [], ' ', label='{:.4f}'.format(value))
+
+			if args.d:
+				legend_data = [
+					data[y_label].iloc[0],
+					data.loc[data[x_label] == args.d[0], y_label].item(),
+					data.loc[data[x_label] == args.d[1], y_label].item(),
+					data[y_label].iloc[-1]]
+				for value in legend_data:
+					ax.plot([], [], ' ', label='{:.4f}'.format(value))
 		#ax.set_title(os.path.basename(input_dir) + ': ' + metric)
 		ax.set_xlabel('Number of processed instances [x1000]')
 		ax.set_ylabel(metric)
@@ -81,10 +90,19 @@ def plot_metrics(input_dir, output_dir):
 				ax.axvline(x=coord, linestyle=(0, (5, 10)), color='k', linewidth=.5)
 				if i % 2 == 1:
 					ax.axvspan(args.d[i], args.d[i - 1], facecolor='lightgray', alpha=0.5)
-		ax.legend(loc='lower center', bbox_to_anchor=(-0.25, 1.02, 1.25, 0.2), ncol=6, mode="expand", borderaxespad=0.,
+
+		if args.d:
+			ax.legend(loc='lower center', bbox_to_anchor=(-0.25, 1.02, 1.25, 0.2), ncol=6, mode="expand", borderaxespad=0.,
 				  frameon=False)
+		else:
+			ax.legend(loc='lower center', bbox_to_anchor=(0.0, 1.02, 1.0, 0.2), ncol=6, mode="expand",
+					  borderaxespad=0., frameon=False)
+
+
 		plt.savefig(os.path.join(output_dir, metric + '.png'), bbox_inches='tight', dpi=96)
 		plt.savefig(os.path.join(output_dir, metric + '.svg'), bbox_inches='tight')
+		if args.d is None:
+			plt.savefig(os.path.join(output_dir, metric + '.eps'), bbox_inches='tight')
 		plt.close()
 
 if args.r:
